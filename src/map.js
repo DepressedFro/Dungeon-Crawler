98e10821bba9f9881ff4f8d43468ca2c0865bcea
Jacob Peyton <jpeyton_96@yahoo.com>
2017-11-13 11:13:50 -0600

    Added Hash Code Description to room.js

--- src/map.js
+++ src/map.js
@@ -2,4 +2,10 @@ export default class Map {
 	constructor(size, type) {
 		this.rooms = [[]];
 	}
+	
+	getEmptyMap(size) {
+		for(x = 0; x < size; x++) {
+			
+		}
+	}
 }
--- src/room.js
+++ src/room.js
@@ -1,3 +1,21 @@
+/* [RA, RS, ND, ED, SD, WD, E1, E2, E3, E4, K, G, T, P1, P2, P3]
+ * RA:Room Art Style
+ * RS:Room Shape
+ * ND:North Door
+ * ED:East Door
+ * SD:South Door
+ * WD:West Door
+ * E1:Enemy Count 1
+ * E2:Enemy Count 2
+ * E3:Enemy Count 3
+ * E4:Enemy Count 4
+ * K: Key
+ * G: Gold/Treasure Type
+ * P1:Puzzle/Riddle Index 1
+ * P2:Puzzle/Riddle Index 2
+ * P3:Puzzle/Riddle Index 3
+ */
+ 
 export default class Map {
 	constructor(roomcode) {
 		this.monsters = [];
