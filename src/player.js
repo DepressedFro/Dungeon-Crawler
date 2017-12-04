b56e9fa294029eafd25f4e7e605fa82dc52b08c3
Filip Volf <volf.filip@gmail.com>
2017-12-04 15:39:02 -0600

    Improved player movement

--- src/player.js
+++ src/player.js
@@ -10,7 +10,10 @@ export default class Player extends GameObject {
 		this.health = health;
 		this.pos = new Vector(x, y);
 		this.velocity = new Vector(0,0);
-		this.speed = 3;
+		this.speed_mult = 0.4;
+		this.friction = 0.9;
+
+		this.lookingLeft = false;
 		this.gold = 0;
 		this.class = 0;
 		this.className = "Warrior";
@@ -36,6 +39,7 @@ export default class Player extends GameObject {
 				break;
 		}
 	}
+
 	inTileCollision() {
 		var xx = Math.floor(this.pos.x / Constants.tileSize);
 		var yy = Math.floor(this.pos.y / Constants.tileSize);
@@ -65,53 +69,74 @@ export default class Player extends GameObject {
 		}
 	}
 
+	//checks collision at slightly different angle specified by i
+	checkColSlightlyDifferent(i,steps){
+		var dir = this.velocity.toAngles()+Math.PI/2*i/steps;
+		var tmpPrevPos = this.pos.clone();
+		this.pos.add(new Vector(Math.cos(dir),Math.sin(dir)).multiply(this.velocity.length()));
+		
+		var col = this.inTileCollision();
+		var ret = false;
+
+		if(col){
+			ret = true;
+			this.pos = tmpPrevPos;
+		}
+		return ret;
+	}
+
 	update(delta){
 		var previous_pos = this.pos.clone();
 
-		if(this.game.pressed['a']){
-			if(this.game.pressed['s']){
-				this.pos.x += -this.speed;
-				this.pos.y += this.speed;
-			} else if (this.game.pressed['w']){
-				this.pos.x += -this.speed;
-				this.pos.y += -this.speed;
-			} else{
-				this.pos.x += -this.speed;
-			}
-		} else if (this.game.pressed['d']) {
-			if(this.game.pressed['s']){
-				this.pos.x += this.speed;
-				this.pos.y += this.speed;
-			} else if (this.game.pressed['w']){
-				this.pos.x += this.speed;
-				this.pos.y += -this.speed;
-			} else{
-				this.pos.x += this.speed;
-			}
-		} else if (this.game.pressed['s']) {
-			this.pos.y += this.speed;
-		} else if (this.game.pressed['w']){
-			this.pos.y += -this.speed;
-		}
+		var l = this.game.pressed['a'] | 0;
+		var r = this.game.pressed['d'] | 0;
+		var u = this.game.pressed['w'] | 0;
+		var d = this.game.pressed['s'] | 0;
 
-		var col = this.inTileCollision();
-		if (col !== null) {
-			this.pos = previous_pos.clone();
+		if(r-l > 0){
+			this.lookingLeft = false;
+		}
+		if(r-l < 0){
+			this.lookingLeft = true;
+		}
+		
+
+		this.velocity.add(new Vector(r-l,d-u).normalize().multiply(this.speed_mult));
+		this.velocity.multiply(this.friction);
+		//this.pos.add(this.velocity);
+
+		var steps = 5;
+		for(var i=0;i<steps;++i){
+			if(!this.checkColSlightlyDifferent(i,steps)){
+				break;
+			} else if(!this.checkColSlightlyDifferent(-i,steps)){
+				break;
+			}
 		}
 	}
 
 	render(ctx) {
+
+		ctx.save();
+		if(this.lookingLeft){
+			ctx.translate(Math.floor(this.pos.x - 8)+Constants.tileSize, Math.floor(this.pos.y - 12));
+			ctx.scale(-1, 1);
+		}else{
+			ctx.translate(Math.floor(this.pos.x - 8), Math.floor(this.pos.y - 12));
+			ctx.scale(1, 1);	
+		}
 		ctx.drawImage(
 			Constants.tileset,
 			14 * Constants.tileSize,
 			15 * Constants.tileSize - 4,
 			Constants.tileSize,
 			Constants.tileSize + 4,
-			Math.floor(this.pos.x - 8),
-			Math.floor(this.pos.y - 12),
+			0,
+			0,
 			Constants.tileSize,
 			Constants.tileSize + 4
 		);
+		ctx.restore();
 		// this.debugDrawBBox(ctx);
 	}
 }
